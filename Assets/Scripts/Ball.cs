80cb483ca590cf0977817a91e76d2825de197f3b
Caner Ürük <caneruruk5@gmail.com>
2025-06-08 18:22:48 +0300

    Added bounce from the walls for ball

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,16 +4,35 @@ public class Ball : MonoBehaviour
 {
     [SerializeField] private float pace;
     [SerializeField] private Vector3 direction;
+    private float minY;
+    private float maxY;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        float halfHeight = Camera.main.orthographicSize;
+        Vector3 cameraPosition = Camera.main.transform.position;
+
+        maxY = cameraPosition.y + halfHeight;
+        minY = cameraPosition.y - halfHeight;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position += direction * pace * Time.deltaTime;
+
+        // Stay in boundaries
+        if (transform.position.y - transform.lossyScale.y / 2 < minY)
+        {
+            transform.position = new Vector3(transform.position.x, minY + transform.lossyScale.y / 2, transform.position.z);
+            direction.y = -direction.y;
+        }
+
+        if (transform.position.y + transform.lossyScale.y / 2 > maxY)
+        {
+            transform.position = new Vector3(transform.position.x, maxY - transform.lossyScale.y / 2, transform.position.z);
+            direction.y = -direction.y;
+        }
     }
 }
