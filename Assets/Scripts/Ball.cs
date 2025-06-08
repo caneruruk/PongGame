7af4a2f9028cda3eec88c8214f6391e16caa11f4
Caner Ürük <caneruruk5@gmail.com>
2025-06-08 19:04:49 +0300

    Added random direction to the ball

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -3,7 +3,10 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     [SerializeField] private float pace;
-    [SerializeField] private Vector3 direction;
+    [SerializeField] private float initialDirectionYMax = 1;
+    [SerializeField] private float initialDirectionYMin = -1;
+    [SerializeField] private string initialDirection = "left";
+    private Vector3 directionVector;
     private float minY;
     private float maxY;
     private float maxX;
@@ -22,37 +25,52 @@ public class Ball : MonoBehaviour
 
         maxX = cameraPosition.x + halfWidth;
         minX = cameraPosition.x - halfWidth;
+
+        SetRandomDirection(initialDirection);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += direction * pace * Time.deltaTime;
+        transform.position += directionVector * pace * Time.deltaTime;
 
         // Stay in boundaries
         if (transform.position.y - transform.lossyScale.y / 2 < minY)
         {
             transform.position = new Vector3(transform.position.x, minY + transform.lossyScale.y / 2, transform.position.z);
-            direction.y = -direction.y;
+            directionVector.y = -directionVector.y;
         }
 
         if (transform.position.y + transform.lossyScale.y / 2 > maxY)
         {
             transform.position = new Vector3(transform.position.x, maxY - transform.lossyScale.y / 2, transform.position.z);
-            direction.y = -direction.y;
+            directionVector.y = -directionVector.y;
         }
 
         // Reset when there is a point
         if (transform.position.x - transform.lossyScale.x / 2 < minX)
         {
             transform.position = Vector3.zero;
-            direction.x = -direction.x;
+            SetRandomDirection("right");
         }
 
         if (transform.position.x + transform.lossyScale.x / 2 > maxX)
         {
             transform.position = Vector3.zero;
-            direction.x = -direction.x;
+            SetRandomDirection("left");
+        }
+    }
+
+    public void SetRandomDirection(string direction)
+    {
+        if (direction == "left")
+        {
+            directionVector = new Vector3(-1, Random.Range(initialDirectionYMin, initialDirectionYMax), 0);
+        }
+
+        if (direction == "right")
+        {
+            directionVector = new Vector3(1, Random.Range(initialDirectionYMin, initialDirectionYMax), 0);
         }
     }
 }
